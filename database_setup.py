a8aa57e0f86aabc863433390a119d2e0cde07c1c
Daniele Caputo <caputo.daniele.n1@gmail.com>
2019-05-15 09:51:15 +0200

    added Flask library

--- database_setup.py
+++ database_setup.py
@@ -14,6 +14,14 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
@@ -26,10 +34,9 @@ class MenuItem(Base):
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
-# We added this serialize function to be able to send JSON objects in a serializable format
     @property
     def serialize(self):
-
+        """Return object data in easily serializeable format"""
         return {
             'name': self.name,
             'description': self.description,
